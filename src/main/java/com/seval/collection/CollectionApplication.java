c100453316d5fc923061405fe54a93ceb4d50d94
rajviji <rajviji@192.168.1.10>
2016-01-14 16:53:07 -0600

    Updated Spring Security using JPA

--- src/main/java/com/seval/collection/CollectionApplication.java
+++ src/main/java/com/seval/collection/CollectionApplication.java
@@ -2,8 +2,13 @@ package com.seval.collection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.seval.collection.domain")
+@EntityScan(basePackages = "com.seval.collection.domain")
 public class CollectionApplication {
 
 	public static void main(String[] args) {
